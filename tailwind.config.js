f805169f9fb91bb17240c518453b5acdc5ed677f
aeesh <adamaisha352@gmail.com>
2024-05-28 16:33:19 +0100

    checkpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'h1': ['2.5rem', {
+          lineHeight: 'normal',
+          fontWeight: 400,
+        }],
+        'h2': ['2rem', {
+          lineHeight: 'normal',
+          fontWeight: 400,
+        }],
+      },
+    },
+    colors: {
+      'background': '#2B2B31',
+      'cardBg': '#C4C4C4',
+      'cardHeader': '#222222',
+      'cardBody': '#333333',
+      'borderBg': 'rgba(255, 255, 255, 0.25)',
+      'btnBg': 'rgba(255, 255, 255, 0.05)',
+    },
   },
   plugins: [],
 }
