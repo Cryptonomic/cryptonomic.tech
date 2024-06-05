71350fe748b3931022f53bcbcb21cc9928301b06
Aisha Opaluwa <47456520+Aeesh@users.noreply.github.com>
2024-06-05 18:47:44 +0100

    Site redesign 2024 (#31)
    * move files to v1 folder
    
    * init tailwind
    
    * checkpoint
    
    * checkpoint
    
    * font added
    
    * - modify spacing to match new ui
    - add footer
    
    * remove top border
    
    * update css
    
    * update footer font
    
    * contact page
    
    * update alt text
    
    * work page
    
    * flex wrap
    
    * persimmon details
    
    * persimmon details css
    
    * link to work details
    
    * rhubarb details
    
    * update rhubarb details
    
    * update font weight
    
    * renamed "figure" tag to "div"
    
    * Monadic DNA details
    
    * Project Guardian
    
    * Institutional DiD details
    
    * arronax details
    
    * Conseil and ConseilJS details
    
    * galleon details
    
    * mininax details
    
    * Nautilus Cloud details
    
    * update link and play video
    
    * added warpcast link to footer
    
    * added warpcast link to footer
    
    * update image placeholders
    
    * modify footer padding
    
    * updated meta description and keywords
    
    * update contact page

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'archivo-black': ['"Archivo Black"', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': ['2.5rem', {
+          lineHeight: 'normal',
+          fontWeight: 400,
+        }],
+        'h2': ['2rem', {
+          lineHeight: 'normal',
+          fontWeight: 400,
+        }],
+        'h3': ['1.5rem'],
+      },
+    },
+    colors: {
+      'background': '#2B2B31',
+      'cardBg': '#C4C4C4',
+      'cardHeader': '#222222',
+      'cardBody': '#333333',
+      'borderBg': 'rgba(255, 255, 255, 0.25)',
+      'btnBg': 'rgba(255, 255, 255, 0.05)',
+      'footerBg': 'rgba(255, 255, 255, 0.02)',
+      'link': '#649DFF',
+    },
+  },
+  plugins: [],
+}
